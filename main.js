01ff6544de3e85c68e5efbcb059098e10e3a126e
Dave <ddawid@windowslive.com>
2020-04-21 20:43:43 +0200

    Create simple hamburger menu

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ console.log('elo!')
 const name = 'Dave Donor';
 const age = 27;
 
-console.log(`Name ${name} Age ${age}`);
+console.log(hello(name, age));
+
+function hello(name, age ) {
+    return `Hi i'm ${name} and my age is ${age}`;
+}
+
+const navigationSwitcher = document.querySelector(".navigation__switcher--js");
+
+navigationSwitcher.addEventListener('click',(e)=> {
+    const navigationList = document.querySelector(".navigation__list--js");
+    navigationList.classList.toggle("navigation__list--visible");
+});
